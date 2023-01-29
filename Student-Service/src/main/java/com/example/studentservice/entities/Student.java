a2391359c4a8e14da869b97e9ebf043c87fdf681
Asmaa Tounsi <83168701+asmaatou@users.noreply.github.com>
2023-01-29 13:31:00 +0100

    1

---
+++ Student-Service/src/main/java/com/example/studentservice/entities/Student.java
@@ -0,0 +1,22 @@
+package com.example.studentservice.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data @AllArgsConstructor @NoArgsConstructor
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String Fname;
+    private String Lname;
+    private String email;
+    private String adress;
+}
