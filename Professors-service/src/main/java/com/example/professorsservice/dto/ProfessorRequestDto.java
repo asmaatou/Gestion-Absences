876d6b58e35eb5eab8e8fb3237c7650463670645
Asmaa Tounsi <83168701+asmaatou@users.noreply.github.com>
2023-02-04 17:49:49 +0100

    4

--- Professors-service/src/main/java/com/example/professorsservice/dto/ProfessorRequestDto.java
+++ Professors-service/src/main/java/com/example/professorsservice/dto/ProfessorRequestDto.java
@@ -1,4 +1,16 @@
 package com.example.professorsservice.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProfessorRequestDto {
+    private Long id;
+    private String Fname;
+    private String Lname;
+    private String email;
+    private String adress;
 }
