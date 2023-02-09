329c9a71b535c9639d1af568fd7fbb173b41647e
HASS-ASFF <hassanasffar22@gmail.com>
2023-02-09 04:57:46 +0100

    add Courses service

--- Courses-service/src/main/java/com/example/coursesservice/repos/CoursesRepo.java
+++ Courses-service/src/main/java/com/example/coursesservice/repos/CoursesRepo.java
@@ -1,4 +1,9 @@
 package com.example.coursesservice.repos;
 
-public interface CoursesRepo {
+import com.example.coursesservice.entities.Courses;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CoursesRepo extends JpaRepository<Courses,Long> {
 }
--- Courses-service/src/main/java/com/example/coursesservice/services/CoursesService.java
+++ Courses-service/src/main/java/com/example/coursesservice/services/CoursesService.java
@@ -1,4 +1,13 @@
 package com.example.coursesservice.services;
 
+import com.example.coursesservice.entities.Courses;
+
+import java.util.List;
+
 public interface CoursesService {
+    List<Courses> findAll();
+    Courses addCourses(Courses courses);
+    Courses findById(Long id);
+    Courses updateCourses(Long id,Courses courses);
+    void deleteCourses(Long id);
 }
