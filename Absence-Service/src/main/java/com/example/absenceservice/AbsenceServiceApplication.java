0a8a2544cca276164f60285ed1a122c5ffd19a13
Asmaa Tounsi <83168701+asmaatou@users.noreply.github.com>
2023-02-09 21:30:56 +0100

    Add Absence-Service

---
+++ Absence-Service/src/main/java/com/example/absenceservice/AbsenceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.absenceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbsenceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbsenceServiceApplication.class, args);
+    }
+
+}
