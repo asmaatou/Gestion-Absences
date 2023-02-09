10899e386896d7693e72f154185e10c472057d96
HASS-ASFF <hassanasffar22@gmail.com>
2023-02-09 04:16:52 +0100

    Micro-service Student

--- Students-service/src/main/java/com/example/studentsservice/service/StudentServiceImpl.java
+++ Students-service/src/main/java/com/example/studentsservice/service/StudentServiceImpl.java
@@ -1,54 +1,37 @@
 package com.example.studentsservice.service;
 
-import com.example.studentservice.dto.StudentRequestDto;
-import com.example.studentservice.dto.StudentResponseDto;
-import com.example.studentservice.entities.Student;
-import com.example.studentservice.mappers.StudentMapper;
-import com.example.studentservice.repos.StudentRepo;
+
+import com.example.studentsservice.entities.Student;
+import com.example.studentsservice.repos.StudentRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
-public class StudentServiceImpl implements StudentService{
+public class StudentServiceImpl implements StudentService {
     @Autowired
     StudentRepo studentRepo;
-    @Autowired
-    StudentMapper studentMapper;
 
     @Override
-    public List<StudentResponseDto> findAll() {
-        return studentRepo.findAll().stream().map(student -> studentMapper.studentToResponse(student)).collect(Collectors.toList());
+    public List<Student> findAll() {
+        return studentRepo.findAll();
     }
 
     @Override
-    public StudentResponseDto addStudent(Long id, StudentRequestDto studentRequestDto) {
-        Student student = studentMapper.requestToStudent(studentRequestDto);
-        student.setFname(studentRequestDto.getFname());
-        student.setLname(studentRequestDto.getLname());
-        student.setEmail(studentRequestDto.getEmail());
-        student.setAdress(studentRequestDto.getAdress());
-        studentRepo.save(student);
-        return studentMapper.studentToResponse(student);
+    public Student addStudent(Student student) {
+        return studentRepo.save(student);
     }
 
     @Override
-    public StudentResponseDto findById(Long id) {
-        Student student = studentRepo.findById(id).orElseThrow(()->new RuntimeException("Student not found"));
-        return studentMapper.studentToResponse(student);
+    public Student findById(Long id) {
+        return studentRepo.findById(id).orElseThrow(()->new RuntimeException("Student not found"));
     }
 
     @Override
-    public StudentResponseDto updateStudent(Long id, StudentRequestDto studentRequestDto) {
+    public Student updateStudent(Long id, Student Student) {
         Student student = studentRepo.findById(id).orElseThrow(()->new RuntimeException("Student Not found"));
-        student.setFname(studentRequestDto.getFname());
-        student.setLname(studentRequestDto.getLname());
-        student.setEmail(studentRequestDto.getEmail());
-        student.setAdress(studentRequestDto.getAdress());
-        studentRepo.save(student);
-        return StudentMapper.studentToResponse(student);
+        return studentRepo.save(student);
     }
 
     @Override
