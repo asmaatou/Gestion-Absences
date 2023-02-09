86d1615de7fe8549f722e6e12c611c3ef642dcb0
Asmaa Tounsi <83168701+asmaatou@users.noreply.github.com>
2023-02-09 21:25:32 +0100

    Dynamic Configuration

--- Gateway-Service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
+++ Gateway-Service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
@@ -2,6 +2,9 @@ package com.example.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -19,6 +22,10 @@ public class GatewayServiceApplication {
 					.route(r -> r.path("/professors/**").uri("lb://PROFESSORS-SERVICE"))
 					.route(r -> r.path("/courses/**").uri("lb://COURSES-SERVICE"))
 					.build();
+	}
+	@Bean
+	DiscoveryClientRouteDefinitionLocator dynamicRoutes(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp) {
+		return new DiscoveryClientRouteDefinitionLocator(rdc, dlp);
 	}
 
 
